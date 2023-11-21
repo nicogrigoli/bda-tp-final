e106fbb489f75743ba30457ad96b67aa5303d985
Santiago Garcia <santiagogarcia97@outlook.com>
2023-11-21 02:29:16 -0300

    graficos reg logistica

--- reg_logistica/etl_reg_logistica.py
+++ reg_logistica/etl_reg_logistica.py
@@ -4,6 +4,144 @@ import csv
 from datetime import datetime
 import numpy
 import numbers
+import matplotlib.pyplot as plt
+import pandas as pd
+from matplotlib.ticker import FuncFormatter
+
+
+def create_grafico_hectareas_mes(datos_superficie_mes):
+    df = pd.DataFrame(datos_superficie_mes)
+
+    # Set the 'mes_nombre' column as the index
+    df.set_index('mes_nombre', inplace=True)
+    df.plot(kind='bar', y='total_hectareas_afectadas')
+
+    # Para sacar la notacion cientifica
+    def no_scientific_notation(x, pos):
+        return '%.0f' % x
+    formatter = FuncFormatter(no_scientific_notation)
+    plt.gca().yaxis.set_major_formatter(formatter)
+
+    # Add a title to the plot
+    plt.title('Total de hectáreas de bosques nativos afectadas por mes a nivel país')
+    # Display the plot
+    plt.xticks(rotation=25)
+    plt.legend().set_visible(False)
+
+    plt.savefig('graficos/1.total_hectareas_afectadas_por_mes.png')
+    plt.cla()
+
+def create_grafico_porcentaje_hectareas_mes(datos_superficie_mes):
+    colors = ['yellowgreen','red','gold','lightskyblue','white','lightcoral','blue','pink', 'darkgreen','yellow','grey','violet','magenta','cyan']
+
+    patches, texts = plt.pie([i['porcentaje'] for i in datos_superficie_mes] , colors=colors, startangle=90, radius=1.2)
+    labels = ['{0} - {1:1.2f} %'.format(i['mes_nombre'], i['porcentaje']) for i in datos_superficie_mes]
+
+    plt.legend(patches, labels, loc='center left', bbox_to_anchor=(-0.5, 0.5), fontsize=8)
+    plt.title('Porcentaje de hectáreas de bosques nativos afectadas por mes a nivel país',)
+
+    plt.savefig('graficos/2.porcentaje_hectareas_afectadas_por_mes.png',bbox_inches='tight')
+    plt.cla()
+
+
+def create_grafico_hectareas_cordoba_anio(datos_incendios):
+    df = pd.DataFrame(datos_incendios)
+
+    # Set the 'anio' column as the index
+    df.set_index('anio', inplace=True)
+    df.plot(kind='bar', y='hectareas_bosques_nativos')
+
+    # Para sacar la notacion cientifica
+    def no_scientific_notation(x, pos):
+        return '%.0f' % x
+    formatter = FuncFormatter(no_scientific_notation)
+    plt.gca().yaxis.set_major_formatter(formatter)
+
+    # Add a title to the plot
+    plt.title('Total de hectáreas afectadas por año para la provincia de Córdoba')
+    # Display the plot
+    plt.xticks(rotation=90)
+    plt.legend().set_visible(False)
+
+    plt.savefig('graficos/3.total_hectareas_afectadas_por_anio.png')
+    plt.cla()
+
+# Line plot
+def create_grafico_hectareas_cordoba_mes(datos_incendios_mes):
+    df = pd.DataFrame(datos_incendios_mes)
+
+    # Set the 'anio_mes' column as the index
+    df.set_index('anio_mes', inplace=True)
+    df.plot(kind='line', y='hectareas_bosques_nativos',)
+
+    # Para sacar la notacion cientifica
+    def no_scientific_notation(x, pos):
+        return '%.0f' % x
+    formatter = FuncFormatter(no_scientific_notation)
+    plt.gca().yaxis.set_major_formatter(formatter)
+
+    # Add a title to the plot
+    plt.title('Total de hectáreas afectadas por mes para la provincia de Córdoba')
+    # Display the plot
+    plt.xticks(rotation=30)
+    plt.legend().set_visible(False)
+    plt.margins(x=0, y=0)
+
+    plt.savefig('graficos/4.total_hectareas_afectadas_por_mes.png')
+    plt.cla()
+
+
+def create_grafico_riesgo_incendio(datos_finales, q1, q3):
+    df = pd.DataFrame(datos_finales)
+
+    # order by 'surface_burnt'
+    df = df.sort_values('hectareas_bosques_nativos')
+
+    # create a list of labels for the 3 parts of the dataset
+    categories = [
+        {
+            'label': 'Riesgo Bajo: ',
+            'color': 'green'
+        },
+        {
+            'label': 'Riesgo Medio: ',
+            'color': 'orange'
+        },
+        {
+            'label': 'Riesgo Alto: ',
+            'color': 'red'
+        }]
+
+    # plot the data
+    fig, ax = plt.subplots()
+    for i, category in enumerate(categories):
+        if i == 0:
+            subset = df[df['hectareas_bosques_nativos'] <= q1]
+        elif i == len(categories) - 1:
+            subset = df[df['hectareas_bosques_nativos'] >= q3]
+        else:
+            subset = df[(df['hectareas_bosques_nativos'] > q1) & (df['hectareas_bosques_nativos'] < q3)]
+        ax.plot(subset['anio_mes'], subset['hectareas_bosques_nativos'],'o', label=category['label'] + str(len(subset)) + ' registros', color=category['color'], markersize=3)
+
+    plt.hlines(y=q1, xmin=0, xmax=len(datos_finales), colors='blue', linestyles='dashed', lw=0.5, label='P50 = ' + str("{:.0f}".format(q1)) + ' hectareas afectadas')
+    plt.hlines(y=q3, xmin=0, xmax=len(datos_finales), colors='blue', linestyles='solid', lw=0.5, label='P85 = ' + str("{:.0f}".format(q3)) + ' hectareas afectadas')
+
+    ax.set_xlabel('Observaciones mensuales ordenadas por hectáreas afectadas')
+    ax.set_ylabel('Cantidad hectáreas afectadas')
+    ax.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
+
+    plt.margins(x=0)
+    plt.legend(bbox_to_anchor=(1.04,0.5), loc="center left", borderaxespad=0)
+
+    plt.savefig('graficos/5.riesgo_incendio.png',bbox_inches='tight')
+    plt.cla()
+
+
+
+
+
+
+
 
 
 # Primero vamos a extraer del dataset sup_bosques_nativos_afectadas_por_mes_acumulado_98_19 el acumulado de hectareas quemadas por mes
@@ -23,13 +161,19 @@ with open('reg_log_datasets/sup_bosques_nativos_afectadas_por_mes_acumulado_98_1
         datos_superficie_mes.append({
             'mes': line_count,  # Los meses estan ordenados en el dataset
             'total_hectareas_afectadas': int(row[2]),
+            'mes_nombre': row[1],
         })
         line_count += 1
+
+create_grafico_hectareas_mes(datos_superficie_mes)
 
 # Calculo el porcentaje de hectareas quemadas por mes en relacion al total de hectareas quemadas en el año
 for mes in datos_superficie_mes:
     mes['porcentaje'] = mes['total_hectareas_afectadas'] / \
         total_hectareas_afectadas * 100
+    
+create_grafico_porcentaje_hectareas_mes(datos_superficie_mes)
+
 
 print('Probabilidad de incendio por mes: ')
 for mes in datos_superficie_mes:
@@ -56,6 +200,7 @@ for row_num, row in enumerate(dataframe_incendios.active.iter_rows(values_only=T
             'hectareas_pastizal': row[6] if isinstance(row[6], numbers.Number) else 0,
         })
 
+create_grafico_hectareas_cordoba_anio(datos_incendios)
 
 # Utilizando los porcentajes de hectareas quemadas por mes, divido los datos de incendio anuales
 # en datos de incendio mensuales. Utilizo solo los datos de BOSQUES NATIVOS
@@ -66,6 +211,8 @@ for dato_incendio in datos_incendios:
             'anio_mes': str(dato_incendio['anio']) + '-' + str(mes['mes']),
             'hectareas_bosques_nativos': dato_incendio['hectareas_bosques_nativos'] * (mes['porcentaje'] / 100),
         })
+
+create_grafico_hectareas_cordoba_mes(datos_incendios_mes)
 
 
 # Agrupo los datos meteorologicos por año y mes
@@ -138,20 +285,21 @@ hectareas_quemadas = [dato['hectareas_bosques_nativos']
 hectareas_quemadas.sort()
 
 # Calculo el percentil 33 y 66 para saber el rango de hectareas quemadas que se considera bajo, medio y alto
-percentil_33 = numpy.percentile(hectareas_quemadas, 33)
-percentil_66 = numpy.percentile(hectareas_quemadas, 66)
+q1 = numpy.percentile(hectareas_quemadas, 50)
+q3 = numpy.percentile(hectareas_quemadas, 85)
 
 for dato in datos_finales:
-    if dato['hectareas_bosques_nativos'] <= percentil_33:
+    if dato['hectareas_bosques_nativos'] <= q1:
         dato['riesgo_incendio'] = 'bajo'
         dato['riesgo_incendio_num'] = 1
-    elif dato['hectareas_bosques_nativos'] > percentil_33 and dato['hectareas_bosques_nativos'] <= percentil_66:
+    elif dato['hectareas_bosques_nativos'] > q1 and dato['hectareas_bosques_nativos'] <= q3:
         dato['riesgo_incendio'] = 'medio'
         dato['riesgo_incendio_num'] = 2
     else:
         dato['riesgo_incendio'] = 'alto'
         dato['riesgo_incendio_num'] = 3
 
+create_grafico_riesgo_incendio(datos_finales, q1, q3)
 
 print('Cantidad de bajos: ', len(
     list(filter(lambda x: x['riesgo_incendio'] == 'bajo', datos_finales))))
@@ -170,3 +318,6 @@ with open('etl_datos_finales_reg_logistica.csv', 'w', newline="") as f:
     for dato in datos_finales:
         c.writerow([dato['anio_mes'], dato['hectareas_bosques_nativos'], dato['avg_temp'], dato['avg_min_temp'], dato['avg_max_temp'],
                    dato['avg_humidity'], dato['avg_rain'], dato['avg_wind_speed'], dato['riesgo_incendio'], dato['riesgo_incendio_num']])
+
+
+
--- reg_logistica/reg_logistica.py
+++ reg_logistica/reg_logistica.py
@@ -4,6 +4,10 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import mean_squared_error
 import csv
 
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
 # Valores de entrada: avg_temp, avg_humidity, avg_rain, avg_wind_speed
 x = []
 
@@ -37,13 +41,13 @@ with open('etl_datos_finales_reg_logistica.csv') as csv_file:
 # tambien registro las observaciones
 mean_squared_errors = []
 observations = []
-for i in range(2000):
+for i in range(1000):
     if i % 10 == 0:
         print(int(i / 10), '%')
 
     # Particiono el dataset, 70% para entrenamiento y 30% para test
     x_train, x_test, y_train, y_test = train_test_split(
-        x, y, test_size=0.10, random_state=None)
+        x, y, test_size=0.30, random_state=None)
 
     # Entreno el modelo con el dataset de entrenamiento
     model = LogisticRegression(
@@ -74,3 +78,32 @@ predicciones_acertadas = len(
 print('Total observaciones: ', len(observations))
 print('Predicciones acertadas: ', predicciones_acertadas)
 print('Porcentaje acierto: ', predicciones_acertadas * 100 / len(observations))
+
+
+
+
+
+data = []
+for i in range(3):
+    data.append({
+        'real_value': i + 1,
+        'Bajo': len(list(filter(lambda x: x['real_value'] == i + 1 and x['predicted_value'] == 1, observations))),
+        'Medio': len(list(filter(lambda x: x['real_value'] == i + 1 and x['predicted_value'] == 2, observations))),
+        'Alto': len(list(filter(lambda x: x['real_value'] == i + 1 and x['predicted_value'] == 3, observations))),
+    })
+
+
+df = pd.DataFrame(data)
+ax = df.plot.bar(x='real_value', stacked=True, title='Observaciones por riesgo de incendio y predicciones', color=['green', 'orange', 'red'])
+ax.set_xticklabels(['Bajo','Medio','Alto'], rotation=0)
+ax.set_xlabel('Valores reales')
+plt.legend( title="Predicciones")
+
+plt.savefig('graficos/6.predicciones.png',bbox_inches='tight')
+plt.cla()
+
+
+print('Porcentaje de acierto por riesgo de incendio')
+print('Bajo: ', data[0]['Bajo'] * 100 / (data[0]['Bajo'] + data[0]['Medio'] + data[0]['Alto']))
+print('Medio: ', data[1]['Medio'] * 100 / (data[1]['Bajo'] + data[1]['Medio'] + data[1]['Alto']))
+print('Alto: ', data[2]['Alto'] * 100 / (data[2]['Bajo'] + data[2]['Medio'] + data[2]['Alto']))
